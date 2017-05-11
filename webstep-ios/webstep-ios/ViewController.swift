5410663425c87fe5998a5de4b9fe57f8bcd33f9b
Agust Rafnsson <agustr@gmail.com>
2017-05-11 18:44:47 +0200

    extra view controller

--- webstep-ios/webstep-ios/ViewController.swift
+++ webstep-ios/webstep-ios/ViewController.swift
@@ -18,7 +18,11 @@ class MyViewController: UIViewController {
         self.title = "Webstep Demo"
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "navigate") {
+            segue.destination.view.backgroundColor = UIColor.brown
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
